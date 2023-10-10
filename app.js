2ffb794ab8d6e73e20a6874359372c5257a264ef
Dema-Alashkar <dede.alashkar@gmail.com>
2023-10-10 15:54:21 +0200

    something something

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-const db = new sqlite3.Database('projects-dema.db')
+const db = require('./db')
 
 /*
 // creates table projects at startup
@@ -137,60 +137,330 @@ db.run("CREATE TABLE IF NOT EXISTS Experience (ExperienceId INTEGER PRIMARY KEY,
 
 
 // CONTROLLER (THE BOSS)
-app.get("/", function (request, response) {
-  response.render("home.handlebars");
+app.get("/", function (req, res) {
+  res.render("home.handlebars");
 });
 
-app.get("/aboutme", (request, response) => {
-  db.all("SELECT * FROM education", (error, theProjects) => {
-    if (error) {
-      const model = {
-        hasError: true,
-        theError: error,
-        projects: []
-      }
-      response.render("projects.handlebars", model)
-    } else {
-      const model = {
-        hasError: false,
-        theError: "",
-        projects: theProjects
-      }
-      response.render("projects.handlebars", model)
+app.get("/about", (req, res) => {
+  model = {
+    errors: {},
+    skills: {},
+    education: {},
+    experience: {}
+  }
+  db.getAllSkill((err, skills) => {
+    if (err) {
+      //return res.status(500).send(err)
+      model.errors += err
+    }
+    else{
+      model.skills = skills
     }
   })
-});
-
-
-app.get("/projects", (request, response) => {
-  db.all("SELECT * FROM projects", (error, theProjects) => {
-    if (error) {
-      const model = {
-        hasError: true,
-        theError: error,
-        projects: []
-      }
-      response.render("projects.handlebars", model)
-    } else {
-      const model = {
-        hasError: false,
-        theError: "",
-        projects: theProjects
-      }
-      response.render("projects.handlebars", model)
+  db.getAllEducation((err, education) => {
+    if (err) {
+      //return res.status(500).send(err)
+      model.errors += err
+      console.log(err)
+    }
+    else{
+      model.education = education
     }
   })
+  db.getAllExperience((err, experience) => {
+    if (err) {
+      //return res.status(500).send(err)
+      model.errors += err
+      console.log(err)
+    }
+    else{
+      model.experience = experience
+    }
+  })
+  res.render('about.handlebars', model)
 });
 
-app.get("/contact", (request, response) => {
-  response.render("contact.handlebars");
+app.get("/contact", (req, res) => {
+  res.render("contact.handlebars");
 });
-
 
 //Renders the login page
 app.get('/login',(req, res) => {
   res.render('login.handlebars');
 });
+
+
+
+app.get("/about/skills/create", (req, res) =>{
+  res.render('createSkill.handlebars')
+});
+
+app.post("/about/skills/create", (req, res) => {
+  const skill = {
+    title: req.body.title,
+    desc: req.body.desc,
+  }
+  db.createSkill(skill.title, skill.desc, function(error){
+    res.redirect('/skills')
+  })
+});
+
+app.get("/about/education/create", (req, res) =>{
+  res.render('createEducation.handlebars')
+});
+
+app.post("/about/education/create", (req, res) => {
+  const project = {
+    name: req.body.name,
+    desc: req.body.desc,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate
+
+  }
+  db.createEducation(education.name, education.desc, education.startDate, education.endDate, function(error){
+    res.redirect('/education')
+  })
+});
+
+app.get("/about/experience/create", (req, res) =>{
+  res.render('createExperience.handlebars')
+});
+
+app.post("/about/experience/create", (req, res) => {
+  const project = {
+    name: req.body.name,
+    desc: req.body.desc,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate
+
+  }
+  db.createExperince(experience.name, experience.desc, experience.startDate, experience.endDate, function(error){
+    res.redirect('/experience')
+  })
+});
+
+app.get("/projects", (req, res) => {
+  db.getAllProjects((err, projects) => {
+    if (err) {
+      return res.status(500).send(err)
+    }
+    else{
+      model = {
+        projects
+      }
+    }
+    console.log("model")
+    console.log(model)
+    res.render('projects.handlebars', model)
+  })
+});
+
+app.get("/projects/create", (req, res) =>{
+  res.render('createProject.handlebars')
+});
+
+app.post("/projects/create", (req, res) => {
+  const project = {
+    title: req.body.title,
+    desc: req.body.desc,
+    img: req.body.image,
+    link: req.body.link,
+    date: req.body.date
+  } 
+  console.log(project)
+  db.createProject(project.title, project.desc, project.img, project.link, project.date, function(error){
+    res.render('projects.handlebars')
+  })
+});
+
+app.get("/projects/:id/update", (req, res) => {
+const id = req.params.id
+
+db.getProjectByID(id, (err, project) => {
+  if (err) {
+    return res.status(500).send(err)
+  }else {
+    const model = {
+      project
+    }
+      res.render('updateProject.handlebars', model)
+  }
+})
+})
+
+app.post('/projects/:id/update', (req, res) => {
+  const id = req.params.id
+  const project = {
+    projectTitle: req.body.projectTitle,
+    projectDesc: req.body.projectDesc,
+    projectImage: req.body.projectImage,
+    projectLink: req.body.projectLink,
+    projectDate: req.body.projectDate
+  }
+
+  if (req.session.isLoggedIn === true && req.session.isAdmin === true) {
+    //const id = req.params.id; // You need to get the id from the request parameters
+    db.updateProjectById(
+      id,
+      project.projectTitle,
+      project.projectDesc,
+      project.projectImage,
+      project.projectLink,
+      project.projectDate,
+      (error) => {
+        if (error) {
+          console.log("ERROR: ", error);
+        } else {
+          console.log("Project updated!");
+          res.redirect('/projects/' + id);
+        }
+      }
+    );
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get("/about/skills/:id/update", (req, res) => {
+  const id = req.params.id
+  
+  db.getSkillsByID(id, (err, skill) => {
+    if (err) {
+      return res.status(500).send(err)
+    }else {
+      const model = {
+        skill
+      }
+      console.log(skill)
+        res.render('updateSkills.handlebars', model)
+    }
+  })
+  });
+
+  app.post('/skills/:id/update', (req, res) => {
+    const id = req.params.id
+    const project = {
+      skillName: req.body.skillName,
+      skillDesc: req.body.skillDesc,
+
+    }
+  
+    if (req.session.isLoggedIn === true && req.session.isAdmin === true) {
+      //const id = req.params.id; // You need to get the id from the request parameters
+      db.updateProjectById(
+        id,
+        skill.skillName,
+        skill.skillDesc,
+        
+        (error) => {
+          if (error) {
+            console.log("ERROR: ", error);
+          } else {
+            console.log("Skill updated!");
+            res.redirect('/Aboutme/' + id);
+          }
+        }
+      );
+    } else {
+      res.redirect('/login');
+    }
+  });
+
+
+  app.get("/educations/:id/update", (req, res) => {
+    const id = req.params.id
+    
+    db.getEducationByID(id, (err, education) => {
+      if (err) {
+        return res.status(500).send(err)
+      }else {
+        const model = {
+          education
+        }
+          res.render('updateEducation.handlebars', model)
+      }
+    })
+    });
+
+
+    app.post('/educations/:id/update', (req, res) => {
+      const id = req.params.id
+      const education = {
+        educationName: req.body.educationName,
+        educationDesc: req.body.educationDesc,
+        educationStartDate: req.body.educationStartDate,
+        educationEndDate: req.body.educationEndDate
+      }
+    
+      if (req.session.isLoggedIn === true && req.session.isAdmin === true) {
+        //const id = req.params.id; // You need to get the id from the request parameters
+        db.updateProjectById(
+          id,
+          education.educationName,
+          education.educationDesc,
+          education.educationStartDate,
+          education.educationEndDate,
+          (error) => {
+            if (error) {
+              console.log("ERROR: ", error);
+            } else {
+              console.log("education updated!");
+              res.redirect('/Aboutme/' + id);
+            }
+          }
+        );
+      } else {
+        res.redirect('/login');
+      }
+    });
+
+
+    app.get("/experiences/:id/update", (req, res) => {
+      const id = req.params.id
+      
+      db.getexperienceByID(id, (err, experience) => {
+        if (err) {
+          return res.status(500).send(err)
+        }else {
+          const model = {
+            experience
+          }
+            res.render('updateExperience.handlebars', model)
+        }
+      })
+      });
+
+      app.post('/experiences/:id/update', (req, res) => {
+        const id = req.params.id
+        const experience = {
+          experienceName: req.body.experienceName,
+          experienceDesc: req.body.experienceDesc,
+          experienceStartDate: req.body.experienceStartDate,
+          experienceEndDate: req.body.experienceEndDate
+        }
+      
+        if (req.session.isLoggedIn === true && req.session.isAdmin === true) {
+          //const id = req.params.id; // You need to get the id from the request parameters
+          db.updateExperienceById(
+            id,
+            experience.experienceName,
+            experience.eexperienceesc,
+            experience.eexperienceStartDate,
+            experience.experienceEndDate,
+            (error) => {
+              if (error) {
+                console.log("ERROR: ", error);
+              } else {
+                console.log("experience updated!");
+                res.redirect('/Aboutme/' + id);
+              }
+            }
+          );
+        } else {
+          res.redirect('/login');
+        }
+      });
+
 
 app.post('/login',(req, res) => {
   const un = req.body.un
@@ -203,7 +473,7 @@ app.post('/login',(req, res) => {
     console.log('Bad user and/or bad password')
     res.redirect('/login')
   }
-})
+});
 
 app.use(function(req,res){
   res.status(404).render('404.handlebars')
--- db.js
+++ db.js
@@ -2,20 +2,19 @@ const sqlite3 = require('sqlite3')
 const database = new sqlite3.Database('portfolio-databases.db')
 
 database.run(`
-    CREATE TABLE IF NOT EXIST Projects(
-        projectid INTEGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS Projects(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         projectTitle TEXT,
         projectDesc TEXT,
         projectImage TEXT,
         projectLink TEXT,
         projectDate TEXT
     )`
-    
 )
 
 database.run(`
-    CREATE TABLE IF NOT EXIST Skills(
-        Skillid INTEGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS Skills(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         SkillName TEXT,
         SkillDesc TEXT
     )`
@@ -23,26 +22,35 @@ database.run(`
 )
 
 database.run(`
-    CREATE TABLE IF NOT EXIST Education(
-        Educationid INTEGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS Education(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         EducationName TEXT,
         EducationDesc TEXT,
         EducationStartDate TEXT,
-        EducationEndDate TEXT,
+        EducationEndDate TEXT
     )`
     
 )
 
 database.run(`
-    CREATE TABLE IF NOT EXIST Experience(
-        Experienceid INTEGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS Experience(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         ExperienceName TEXT,
         ExperienceDesc TEXT,
         ExperienceStartdate TEXT,
-        ExperienceEndDate TEXT,
+        ExperienceEndDate TEXT
     )`
     
 )
+exports.createProject = function(title, desc, img,link, date, callback) {
+    const query = "INSERT INTO Projects (projectName, projectDesc, projectImage, projectLink, projectDate) VALUES (?,?,?,?,?)"
+    const values = [title, desc, img,link, date]
+    console.log(query)
+    console.log(values)
+    database.run(query,values,function(error){
+        callback(error)
+    })
+}
 
 exports.getAllProjects = function(callback){
     const query = "SELECT * From Projects ORDER BY id DESC"
@@ -51,5 +59,163 @@ exports.getAllProjects = function(callback){
         callback(error, res)
     })
 }
+exports.getProjectByID = function(id, callback){
+    const query = "SELECT * FROM Projects WHERE id = ? LIMIT 1"
+    const value = id
+
+    database.get(query, value, function(error, res){
+        callback(error, res)
+    }) 
+}
+
+exports.updateProjectById = function(id, projectTitle, projectDesc, projectImage, projectLink, projectDate, callback ){
+    const query = "UPDATE Projects SET projectTitle = ?, projectDesc = ?, projectImage = ?, projectLink = ?, projectDate = ?, WHERE id = ?"
+    const values = [projectTitle, projectDesc, projectImage, projectLink, projectDate, id]
+
+    database.run(query, values, function (error){
+        callback(error)
+    })
+}
+
+exports.deleteProjectById = function(id, callback){
+    const query = "DELETE FROM Projects WHERE id = ?"
+    const value = id
+
+    database.run(query, value, function(error, res){
+        callback(error, res)
+    })
+}
+// Skills
+exports.createSkill = function(Skillname, Skilldesc, callback) {
+    const query = "INSERT INTO Skills (SkillName, skillDesc)"
+    const values = [Skillname, Skilldesc]
+    database.run(query,values,function(error){
+        callback(error)
+    })
+}
 
 
+exports.getAllSkill = function(callback){
+    const query = "SELECT * From Skill ORDER BY id DESC"
+
+    database.all(query, function(error, res){
+        callback(error, res)
+    })
+}
+
+exports.getSkillByID = function(id, callback){
+    const query = "SELECT * FROM Skill WHERE id = ? LIMIT 1"
+    const value = id
+
+    database.get(query, value, function(error, res){
+        callback(error, res)
+    })
+}
+
+exports.updateSkillById = function(Skillid , SkillName, SkillDesc, callback){
+    const query = "UPDATE Skills SET SkillName = ?, SkillDesc = ?, WHERE id = ?"
+    const values = [SkillName, SkillDesc, Skillid]
+
+    database.run(query, values, function (error){
+        callback(error)
+    })
+}
+
+exports.deleteSkillById = function(id, callback){
+    const query = "DELETE FROM Skills WHERE id = ?"
+    const value = id
+
+    database.run(query, value, function(error, res){
+        callback(error, res)
+    })
+}
+//Education
+
+exports.createEducation = function(EducationName, EducationDesc, EducationStartDate, EducationEndDate, callback) {
+    const query = "INSERT INTO Education (EducationName, EducationDesc, EducationStartDate, EducationEndDate)"
+    const values = [EducationName, EducationDesc, EducationStartDate, EducationEndDate]
+    database.run(query,values,function(error){
+        callback(error)
+    })
+}
+
+
+exports.getAllEducation = function(callback){
+    const query = "SELECT * From Education ORDER BY id DESC"
+
+    database.all(query, function(error, res){
+        callback(error, res)
+    })
+}
+
+exports.getEducationByID = function(id, callback){
+    const query = "SELECT * FROM Education WHERE id = ? LIMIT 1"
+    const value = id
+
+    database.get(query, value, function(error, res){
+        callback(error, res)
+    })
+}
+
+exports.updateEducationById = function(Educationid, EducationName, EducationDesc, EducationStartDate, EducationEndDate, callback){
+    const query = "UPDATE Education SET EducationName = ?, EducationDesc = ?, EducationStartDate = ?, EducationEndDate = ?, WHERE id = ?"
+    const values = [EducationName, EducationDesc, EducationStartDate, EducationEndDate, Educationid]
+
+    database.run(query, values, function (error){
+        callback(error)
+    })
+}
+
+exports.deleteEducationById = function(id, callback){
+    const query = "DELETE FROM Education WHERE id = ?"
+    const value = id
+
+    database.run(query, value, function(error, res){
+        callback(error, res)
+    })
+}
+
+//Experince
+
+exports.createExperince = function(ExperinceName, ExperinceDesc, EExperinceStartDate, ExperinceEndDate, callback) {
+    const query = "INSERT INTO Experince (EducationName, EducationDesc, EducationStartDate, EducationEndDate)"
+    const values = [ExperinceName, ExperinceDesc, EExperinceStartDate, ExperinceEndDate]
+    database.run(query,values,function(error){
+        callback(error)
+    })
+}
+
+exports.getAllExperience = function(callback){
+    const query = "SELECT * From Experience ORDER BY id DESC"
+
+    database.all(query, function(error, res){
+        callback(error, res)
+    })
+}
+
+exports.getExperienceByID = function(id, callback){
+    const query = "SELECT * FROM Experience WHERE id = ? LIMIT 1"
+    const value = id
+
+    database.get(query, value, function(error, res){
+        callback(error, res)
+    })
+}
+
+exports.updateExperienceById = function(id , ExperienceName, ExperienceDesc, ExperienceStartdate, ExperienceEndDate, callback){
+    const query = "UPDATE Experience SET ExperienceName = ?, ExperienceDesc = ?, ExperienceStartdate = ?, ExperienceEndDate = ?, WHERE id = ?"
+    const values = [ExperienceName, ExperienceDesc, ExperienceStartdate, ExperienceEndDate, id]
+
+    database.run(query, values, function (error){
+        callback(error)
+    })
+}
+
+exports.deleteExperienceById = function(id, callback){
+    const query = "DELETE FROM Experience WHERE id = ?"
+    const value = id
+
+    database.run(query, value, function(error, res){
+        callback(error, res)
+    })
+}
